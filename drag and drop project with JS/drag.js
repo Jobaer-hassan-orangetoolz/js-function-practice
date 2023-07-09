e202456f529ff5199734e0328c375b335e0f53d5
Jobaer-hassan-orangetoolz <jobaer@orangetoolz.com>
2023-07-09 17:36:27 +0600

    added drag and drop

--- drag and drop project with JS/drag.js
+++ drag and drop project with JS/drag.js
@@ -1,7 +1,9 @@
 let btn = document.getElementById('btn');
 let inp = document.getElementById('inp');
 let boxs = document.querySelectorAll('.box');
+let size = 0;
 let drag =  null;
+let boxx = null;
 btn.onclick = function(){
     if(inp.value!= '')
     {
@@ -15,6 +17,7 @@ btn.onclick = function(){
 function dragItems()
 {
     let items = document.querySelectorAll('.item');
+    // let size = items.length;
     items.forEach(item => {
         item.addEventListener('dragstart', function(){
             drag = item;
@@ -26,19 +29,37 @@ function dragItems()
         })
     });
     boxs.forEach(box=>{
+        refresh(box);
         box.addEventListener('dragover',function(e){
+            console.log('dragover')
+            refresh(box)
             e.preventDefault()
             this.style.background = '#090';
             this.style.color = '#fff';
+            
+            
         })
         box.addEventListener('dragleave',function(){
+            console.log('dragleave')
+            refresh(box);
+            boxx = box;
             this.style.background = '#fff';
             this.style.color = '#000';
         })
         box.addEventListener('drop', function(){
+            console.log('drop')
+            refresh(box)
             box.append(drag);
             this.style.background = '#fff';
             this.style.color = '#000';
         })
+        
     })
+   
+}
+function refresh(box)
+{
+    let sz = box.querySelectorAll('.item');
+    let len = sz.length;
+    box.querySelector('#listCount').innerHTML = len;
 }
